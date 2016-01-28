ef2f304f631e5c487847275ef45bbb26adb51ca5
daniel <danielmonr@gmail.com>
2016-01-28 10:46:12 -0600

    commit

--- examenes/apuntadores_funciones.c
+++ examenes/apuntadores_funciones.c
@@ -23,32 +23,59 @@
 
 enum funcion {begin, end, next, prev};				/* ----------  end of enum funcion  ---------- */
 
+struct iter {
+	void* a;
+	size_t t;
+	int tam;
+	int pos;
+};				/* ----------  end of struct iter  ---------- */
+
+typedef struct iter Iter;
+
 typedef enum funcion Funcion;
 
-typedef void* (*t_ite)(void *, size_t, Funcion, int);
+typedef Iter (*t_ite)(Iter, size_t, Funcion, int);
 
-void Recorre(void*, t_ite, size_t);
+Iter it_back(Iter, size_t, Funcion, int);
+Iter it_bi(Iter, size_t, Funcion, int);
+Iter it_f(Iter, size_t, Funcion, int);
+void Recorre(Iter, t_ite, size_t, int);
 
 * ===  FUNCTION MAIN ===================================================================*/
-int main ( int argc, char *argv[] ){
+int main ( int argc, char *argv[] ){a
+	t_ite * iteradores = (t_ite *) malloc (3 * sizeof(t_ite));
+	*iteradores = it_f;
+	*(iteradores+1) = it_back;
+	*(iteradores+2) = it_bi;
+
+
 	return EXIT_SUCCESS;
 }
 
 /* ----------  end of function main  ---------- */
 
-void* it_f(void* a, size_t t, Funcion f, int c){
+Iter it_f(Iter a, size_t t, Funcion f, int c){
 	switch (f){
 		case 0:
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+
 			return a;
 			break;
 		case 1:
-			return (a+(c*t));
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
 			break;
 		case 2:
-			return (a+t);
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
 			break;
 		case 3:
-			return (a-t);
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
 			break;
 		default:
 			return a;
@@ -56,19 +83,57 @@ void* it_f(void* a, size_t t, Funcion f, int c){
 	}
 }
 
-void * it_back(void* a, size_t t, Funcion f, int c){
+Iter it_back(Iter a, size_t t, Funcion f, int c){
 	switch (f){
 		case 0:
-			return (a+(c*t));
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
 			break;
 		case 1:
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
 			return a;
 			break;
 		case 2:
-			return (a+(c*t)-t);
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < tam) ? a.pos + 1 : a.pos;
+			return a;
 			break;
 		case 3:
-			return (a+(c*t)+t);
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
+			break;
+		default:
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
+			break;
+	}
+}
+
+Iter it_bi(Iter a, size_t t, Funcion f, int c){
+	switch (f){
+		case 0:
+			a.a = a.a - (a.pos*a.t);
+			a.pos = (a.pos < a.tam) ? a.pos = 0 : a.pos;
+			return a;
+			break;
+		case 1:
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
+			break;
+		case 2:
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
+			break;
+		case 3:
+			a.a = (a.pos < a.tam) ? a.a + t : a.a;
+			a.pos = (a.pos < a.tam) ? a.pos + 1 : a.pos;
+			return a;
 			break;
 		default:
 			return a;
@@ -76,26 +141,6 @@ void * it_back(void* a, size_t t, Funcion f, int c){
 	}
 }
 
-void * it_bi(void* a, size_t t, Funcion f, int c){
-	switch (f){
-		case 0:
-			return (a+(c*t));
-			break;
-		case 1:
-			return a;
-			break;
-		case 2:
-			return (a+(c*t)-t);
-			break;
-		case 3:
-			return (a+(c*t)+t);
-			break;
-		default:
-			return a;
-			break;
-	}
-}
-
-void Recorre(void* a, t_ite, size_t t){
+void Recorre(void* a, t_ite, size_t t, int tam){
 
 }
