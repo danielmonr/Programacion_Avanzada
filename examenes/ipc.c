ed0875865e2770d65bfb19c846407f0e78fbb827
daniel <danielmonr@gmail.com>
2016-02-15 10:02:33 -0600

    examen rapido 4

---
+++ examenes/ipc.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ipc.c
+ *
+ *    Description:  Examen rapido de comunicacion de procesos, FIFOS y PIPES.
+ *
+ *        Version:  1.0
+ *        Created:  15/02/16 10:00:49
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), danielmonr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+#include    <unistd.h>
+
+/* ===  FUNCTION MAIN ===================================================================*/
+int main ( int argc, char *argv[] ){
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
