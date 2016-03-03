1fe571297542b4087450fc4d545125b7b73985a3
daniel <danielmonr@gmail.com>
2016-03-03 10:02:28 -0600

    Examen senales

---
+++ examenes/senales.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  senales.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/03/16 10:00:59
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), danielmonr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+
+/* ===  FUNCTION MAIN ===================================================================*/
+int main ( int argc, char *argv[] ){
+
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
