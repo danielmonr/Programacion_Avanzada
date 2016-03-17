2ae420f764a6ea30785a51fda05e133bd36eb8bd
daniel <danielmonr@gmail.com>
2016-03-16 19:25:04 -0600

    Ejercicio filosofos

---
+++ concurrencia/filosofos.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  filosofos.c
+ *
+ *    Description:  Ejercicio de filosofos. Una mesa, 4 sillas, 4 platos y tenedores, 3 cuchillos.
+ *
+ *        Version:  1.0
+ *        Created:  16/03/16 19:22:57
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), danielmonr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include	<stdlib.h>
+#include    <stdio.h>
+#include    <unistd.h>
+#include    <pthread.h>
+
+#define Lugares 4
+#define Tenedores 4
+#define Cuchillos 3
+
+/* ===  FUNCTION MAIN ===================================================================*/
+int main ( int argc, char *argv[] ){
+
+
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
