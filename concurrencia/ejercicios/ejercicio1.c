35f14dffb5bb029ebf7f25d23967b2f91e8c5f9d
daniel <danielmonr@gmail.com>
2016-03-17 10:21:46 -0600

    filosofos mejorado

---
+++ concurrencia/ejercicios/ejercicio1.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ejercicio1.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  17/03/16 10:04:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), danielmonr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
--- concurrencia/filosofos.c
+++ concurrencia/filosofos.c
@@ -24,10 +24,41 @@
 #define Lugares 4
 #define Tenedores 4
 #define Cuchillos 3
+#define N 10
+
+
+struct filosofos {
+	int n;
+};				/* ----------  end of struct filosofos  ---------- */
+
+typedef struct filosofos Filosofos;
+
+void* comer(void*);
+
+sem_t sillas;
+sem_t cuchillos;
 
 /* ===  FUNCTION MAIN ===================================================================*/
 int main ( int argc, char *argv[] ){
+	sem_init(&sillas, 0, Lugares);
+	pthread_t fil[N];
 
+	int i = 0;
+	for (i; i < N; ++i){
+		printf("Creando al filosofo n%d...\n", i);
+		if(pthread_create(&fil[i], 0, (void*(*)(void*))comer, (void*)i))
+			return -1;
+	}
 
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
+
+void* comer(int info){
+	printf("Filosofo %d esperando lugar...\n", info);
+	sem_wait(&sillas);
+	printf("Filosofo %d esperando cuchillo...\n", info);
+	sem_wait(&cuchillos);
+	printf("Filosofo %d comiendo...\n", info);
+
+	pthread_exit((void *)info);
+}
