ad62952c4be77739ca9e5a9424494b7c17c22750
daniel <danielmonr@gmail.com>
2016-03-31 10:55:04 +1200

    Examen de concurrencia avanzado

---
+++ concurrencia/tarea/ejercicio3.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ejercicio3.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  29/03/16 19:56:28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), danielmonr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+
+#define N 4
+
+/* ===  FUNCTION MAIN ===================================================================*/
+int main ( int argc, char *argv[] ){
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
---
+++ examenes/concurrencia_avanzado.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  concurrencia_avanzado.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  31/03/16 10:52:04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), danielmonr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+
+/* ===  FUNCTION MAIN ===================================================================*/
+int main ( int argc, char *argv[] ){
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
