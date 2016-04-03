8e2d0636718ced5016297b8ab4b1baa5709a8234
daniel <danielmonr@gmail.com>
2016-04-04 11:07:04 +1200

    ejercicio keys

---
+++ concurrencia/keys.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  keys.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/04/16 11:05:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), danielmonr@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+
+/* ===  FUNCTION MAIN ===================================================================*/
+int main ( int argc, char *argv[] ){
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
+
