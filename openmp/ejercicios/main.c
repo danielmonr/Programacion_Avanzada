594c342595f411e4edd26480ed965ed5d08f0f5f
daniel <danielmonr@gmail.com>
2016-04-25 11:12:53 -0500

    Proyecto final

--- openmp/ejercicios/main.c
+++ openmp/ejercicios/main.c
@@ -26,7 +26,7 @@
 struct punto {
 	float x;
 	float y;
-	struct punto* c;
+	unsigned short ref;
 };				/* ----------  end of struct punto_t  ---------- */
 
 typedef struct punto punto_t;
@@ -35,8 +35,9 @@ typedef struct punto punto_t;
 int main ( int argc, char *argv[] ){
 	srand((unsigned)time(NULL));
 	punto_t * arreglo = (punto_t *) malloc (N * sizeof(punto_t));
-	printf("el tamano es %d\n", sizeof(punto_t));
-	int i;
+	//printf("el tamano es %d\n", sizeof(punto_t));
+	int i, contx1, conty1, contx2, conty2, cont1, cont2;
+	contx1 = contx2 = conty1 = conty2 = 0;
 	float** distancias = (float**) malloc (N*sizeof(float*));
 	punto_t* c1 = (punto_t*) malloc (sizeof(punto_t));
 	punto_t* c2 = (punto_t*) malloc (sizeof(punto_t));
@@ -59,15 +60,51 @@ int main ( int argc, char *argv[] ){
 
 	}
 
-#pragma omp parallel shared(arreglo, distancias, c1, c2) private(i)
-	{
-#pragma omp for nowait
-		for(i = 0; i < N; ++i){
-			distancias[i][0] = (c1->x - arreglo[i].x)/(c1->y - arreglo[i].y);
-			distancias[i][1] = (c2->x - arreglo[i].x)/(c2->y - arreglo[i].y);
+	while((c1->x + c1->y) > 0.01 && (c2->x + c2->y) > 0.01){
+		printf("ok\n");
+
+#pragma omp parallel shared(arreglo, distancias, c1, c2, cont1, cont2) private(i)
+		{
+#pragma omp for nowait reduction(+: cont1, cont2)
+			for(i = 0; i < N; ++i){
+				distancias[i][0] = (c1->x - arreglo[i].x)/(c1->y - arreglo[i].y);
+				distancias[i][1] = (c2->x - arreglo[i].x)/(c2->y - arreglo[i].y);
+				if ( distancias[i][0] < distancias[i][1]){
+					arreglo[i].ref = 1;
+					cont1++;
+				}
+				else{
+					arreglo[i].ref = 2;
+					cont2++;
+				}
+			}
+#pragma omp barrier
+#pragma omp for reduction (+: contx1, conty1, contx2, conty2)
+			for(i = 0; i < N; ++i){
+				if(arreglo[i].ref == 1){
+					contx1 += arreglo[i].x;
+					conty1 += arreglo[i].y;
+				}
+				else{
+					contx2 += arreglo[i].x;
+					conty2 += arreglo[i].y;
+				}
+			}
+#pragma omp barrier
+#pragma omp master
+			{
+				c1->x = contx1 / cont1;
+				c1->y = conty1 / cont1;
+				c2->x = contx2 / cont2;
+				c2->y = conty2 / cont2;
+				printf("C1 x= %f y = %f, C2 x= %f y = %f\n", c1->x, c1->y, c2->x, c2->y);
+				//contx1 = contx2 = conty1 = conty2 = cont1 = cont2 = 0;
+			}
+
 		}
-#pragma omp 
 	}
+
+	printf("C1 x= %f y = %f, C2 x= %f y = %f\n", c1->x, c1->y, c2->x, c2->y);
 
 	printf("Acabo\n");
 	free(distancias);
